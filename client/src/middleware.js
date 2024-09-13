3010a5b8d0d790555282d85f8629ea6e661754c2
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2024-09-13 15:53:24 +0530

    Update middleware.js

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -4,28 +4,22 @@ import axios from 'axios';
 export async function middleware(request) {
   const token = request.cookies.get('Token')?.value || '';
   
-  // Define the URLs
   const loginUrl = new URL('/auth/login', request.url);
   const dashboardUrl = new URL('/dashboard', request.url);
 
   try {
-    // Verify token by making a request to the backend
     const res = await axios.post('http://localhost:8000/auth/verifyToken', {}, {
       headers: {
         Cookie: `Token=${token}`,
-        // Optional: Add credentials if necessary
-        // withCredentials: true
       }
     });
 
     if (res.status === 200 && res.data === 'User is authorized') {
-      // Token is verified, redirect to dashboard if accessing root URL
       if (request.nextUrl.pathname === '/') {
         return NextResponse.redirect(dashboardUrl);
       }
       return NextResponse.next();
     } else {
-      // Token verification failed, redirect to login
       return NextResponse.redirect(loginUrl);
     }
   } catch (error) {
@@ -34,7 +28,6 @@ export async function middleware(request) {
   }
 }
 
-// Apply the middleware to all paths
 export const config = {
   matcher: ['/', '/dashboard'],
 };
